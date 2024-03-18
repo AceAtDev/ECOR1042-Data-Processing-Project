72cc9e1d76610f7991de8b1a93055136a4e3ee7f
Mohamed Elshoubky <aceatdeveloping@gmail.com>
2024-03-18 10:27:15 -0400

    download project files

---
+++ lab 4/test4.py
@@ -0,0 +1,38 @@
+# ECOR 1042 Lab 4 - Individual submission for test4 function
+
+#import check module here
+
+#import load_data module here
+
+# Update "" with your name (e.g., Cristina Ruiz Martin)
+__author__ = ""
+
+# Update "" with your student number (e.g., 100100100)
+__student_number__ = ""
+
+# Update "" with your team (e.g. T-102, use the notation provided in the example)
+__team__ = ""
+
+#==========================================#
+#Do not modify the code already provided.
+
+def test_calculate_health():
+    #Complete the function with your test cases
+    
+    #test that the function does not change the lenght of the list provided as input parameter (5 different test cases required)
+    
+    #test that the function returns an empty list when it is called whith an empty list
+    
+    
+    #test that the function inscrememnts the number of keys of the dictionary inside the list by one  (5 different test cases required)
+    
+    
+    #test that the Health value is properly calculated  (5 different test cases required)
+    
+    
+   
+    
+    check.summary()
+
+
+# Do NOT include a main script in your submission
---
+++ lab 4/test_load_data.py
@@ -0,0 +1,31 @@
+# ECOR 1042 Lab 4 - team submission
+
+#import check module here
+
+#import load_data module here
+
+# Update "" with your the name of the active members of the team
+__author__ = ""
+
+# Update "" with your student number (e.g., 100100100)
+
+# Update "" with your team (e.g. T-102, use the notation provided in the example)
+__team__ = ""
+
+#==========================================#
+
+# Place test_return_list function here 
+
+
+# Place test_return_list_correct_length function here
+
+
+#Place test_return_correct_dict_inside_list function here
+
+
+
+#Place test_calculate_health function here
+
+
+
+# Do NOT include a main script in your submission
