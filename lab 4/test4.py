fa2a0664e520057d389aa437b5cc73910bad9b8f
Mohamed Elshoubky <aceatdeveloping@gmail.com>
2024-03-22 12:20:52 -0400

    test 4 complete

--- lab 4/test4.py
+++ lab 4/test4.py
@@ -67,18 +67,79 @@ def test_calculate_health():
 
     #
     #
-    # test that the function inscrememnts the number of keys of the dictionary inside the list by one  (5 different
+    # test that the function increments the number of keys of the dictionary inside the list by one  (5 different
     # test cases required)
 
+    increments = ld.load_data(file_name, ("All", ""))[0].keys()
+    increments_health = ld.calculate_health(ld.load_data(file_name, ("All", "")))[0].keys()
+    check.equal(len(increments_health), len(increments) + 1,
+                f"List size ({len(increments_health)}) != {len(increments) + 1}")
+
+    try:
+        luck_increments = ld.character_luck_list(file_name, 0.45)[0].keys()
+        luck_increments_health = ld.calculate_health(ld.character_luck_list(file_name, 0.45))[0].keys()
+        check.equal(len(luck_increments_health), len(luck_increments) + 1,
+                    f"List size ({len(luck_increments_health)}) != {len(luck_increments) + 1}")
+    except:
+        check.equal(True, True)
+    else:
+        check.equal(False, True)
+
+    try:
+        str_increments = ld.character_strength_list(file_name, (2, 10))[0].keys()
+        str_increments_health = ld.calculate_health(ld.character_strength_list(file_name, (2, 10)))[0].keys()
+        check.equal(len(str_increments_health), len(str_increments) + 1,
+                    f"List size ({len(str_increments_health)}) != {len(str_increments) + 1}")
+    except:
+        check.equal(True, True)
+    else:
+        check.equal(False, True)
+
+    occ_increments = ld.character_occupation_list(file_name, "AT")[0].keys()
+    occ_increments_health = ld.calculate_health(ld.character_occupation_list(file_name, "AT"))[0].keys()
+    check.equal(len(occ_increments_health), len(occ_increments) + 1,
+                f"List size ({len(occ_increments_health)}) != {len(occ_increments) + 1}")
+
+    weapon_increments = ld.character_weapon_list(file_name, "Staff")[0].keys()
+    weapon_increments_health = ld.calculate_health(ld.character_weapon_list(file_name, "Staff"))[0].keys()
+    check.equal(len(weapon_increments_health), len(weapon_increments) + 1,
+                f"List size ({len(weapon_increments_health)}) != {len(weapon_increments) + 1}")
 
     #
     #
     # test that the Health value is properly calculated  (5 different test cases required)
-    health_list = ld.calculate_health(all_data)
+    character = ld.calculate_health(ld.load_data(file_name, ("All", "")))
+    character = character[0]
 
+    character_health = character.get("Strength") + character.get("Agility") + character.get("Stamina") + character.get(
+        "Personality") + character.get("Intelligence") + (character.get("Armor") ** 2 * character.get("Luck"))
+
+    check.equal(character.get('Health'), character_health, f"got {character}, expected {character_health}")
+
+    occ_character = ld.calculate_health(ld.character_occupation_list(file_name, "AT"))[0]
+    check.equal(occ_character.get("Health"), character_health, f"got {occ_character}, expected {character_health}")
+
+    try:
+        luck_character = ld.calculate_health(ld.character_luck_list(file_name, 0.5))[0]
+    except:
+        check.equal(True, True)
+    else:
+        check.equal(False, True)
+
+    try:
+        str_character = ld.calculate_health(ld.character_strength_list(file_name, (4, 10)))[0]
+    except:
+        check.equal(True, True)
+    else:
+        check.equal(False, True)
+
+    weapon_character = ld.calculate_health(ld.character_weapon_list(file_name, "Staff"))[0]
+    check.equal(weapon_character.get("Health"), character_health,
+                f"got {weapon_character}, expected {character_health}")
 
     check.summary()
 
 
+test_calculate_health()
 
 # Do NOT include a main script in your submission
