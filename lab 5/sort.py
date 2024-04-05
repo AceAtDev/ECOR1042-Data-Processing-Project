d934810482045339c38ae097b9915dd35c590abf
Mohamed Elshoubky <aceatdeveloping@gmail.com>
2024-04-05 08:44:44 -0400

    init lab 5 and 6

---
+++ lab 5/sort.py
@@ -0,0 +1,31 @@
+# ECOR 1042 Lab 5 - Team submission
+# Remember to include docstring and type annotations for your functions
+
+# Update "" to list all students contributing to the team work
+__author__ = ""
+
+# Update "" with your team (e.g. T102)
+__team__ = ""
+
+#==========================================#
+# Place your sort_characters_agility_bubble function after this line
+
+#==========================================#
+# Place your sort_characters_intelligence_selection function after this line
+
+
+#==========================================#
+# Place your sort_characters_health_insertion function after this line
+
+
+#==========================================#
+# Place your sort_characters_armor_bubble function after this line
+
+
+#==========================================#
+# Place your sort function after this line
+
+
+# Do NOT include a main script in your submission
+
+
---
+++ lab 6/text_UI.py
@@ -0,0 +1,12 @@
+# ECOR 1042 Lab 6 - Template text UI
+# Update "" with your name (e.g., Cristina Ruiz Martin)
+__author__ = ""
+
+# Update "" with your student number (e.g., 100100100)
+__student_number__ = ""
+
+# Update "" with your team (e.g. T-102, use the notation provided in the example)
+__team__ = ""
+
+#==========================================#
+# Place your script for your text_UI after this line
