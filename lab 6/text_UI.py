2a239086e36f116e9baaebe19e5989cda90f53c8
Wasabi486 <130076919+Wasabi486@users.noreply.github.com>
2024-04-08 16:34:03 -0400

    Update text_UI.py comments

--- lab 6/text_UI.py
+++ lab 6/text_UI.py
@@ -20,6 +20,7 @@ def text_UI():
     data = []
     while(loop == True):
         try:
+            #bolded visuals and general UI
             print("The available commands are:")
             print("\033[1mL)\033[0m oad Data")
             print("\033[1mS)\033[0m ort Data")
@@ -31,57 +32,56 @@ def text_UI():
             user = user.capitalize()
 
             #check user input for option selected
-            if(user == 'E'):
+            if(user == 'E'): #end code
                 loop = False
                 break
             elif(user == 'L'):
                 file = input("Please enter the name of your file: ")
                 try:
                     attribute = input("Please enter the attribute to use as a filter: ")
+                
+                    if(attribute != 'All'):
+                        value = input("Please enter the value of the attribute: ")
+                        data = load_data.calculate_health(load_data.load_data(file, (attribute, value)))
+                    elif(attribute == 'All'):
+                        data = load_data.calculate_health(load_data.load_data(file, (attribute, '')))
+
+                    if(data != []):
+                        print('Data loaded')
                 except KeyError:
                     print('Invalid Key...')
-
-                if(attribute != 'All'):
-                    value = input("Please enter the value of the attribute: ")
-                    data = load_data.calculate_health(load_data.load_data(file, (attribute, value)))
-                elif(attribute == 'All'):
-                    data = load_data.calculate_health(load_data.load_data(file, (attribute, '')))
-
-                if(data != []):
-                    print('Data loaded')
                 
                 
-            #check if file has been loaded first
+            
             else:
-                if(user not in ('S', 'C', 'H', 'E')):
+                if(user not in ('S', 'C', 'H', 'E')): #if not selected options
                     print('Invalid Command.')
-                elif data == []:
+                elif data == []: #check if file has been loaded first
                     print('File not loaded. Please, load a file first.')
-                elif(user == 'S'):
+                elif(user == 'S'): #sort function
                     attribute = input("Please enter the attribute to sort: 'Agility', 'Armor', 'Intelligence', 'Health': ")
                     if(attribute in ('Agility','Armor','Intelligence','Health')):
                         order = input("Ascending (A) or Descending (D) order: ").capitalize()
 
                         data = sort.sort(data, order, attribute)
-                        display = input("Data Sorted. Do you want to display the data?: ")
+                        display = input("Data Sorted. Do you want to display the data?: ").capitalize()
                     
-                    if(display == 'Y'):
+                    if(display == 'Y'): #display data
                         print(data)
 
-                elif(user == 'C'):
+                elif(user == 'C'): #curve fit function
                     attribute = input("Please enter the attribute you want to use to find the best fit for Health: ")
                     order = input("Please enter the order of the polynomial to be fitted: ")
                     poly = curve_fit.curve_fit(data, attribute, order)
                     print(poly)
 
-                elif(user == 'H'):
+                elif(user == 'H'): #histogram function
                     attribute = input("Please enter the attribute you want to use for plotting: ")
                     histogram.histogram(data, attribute)
 
 
         except FileNotFoundError:
             print("File not found.")
-
 
 
 text_UI()
